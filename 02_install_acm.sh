32ecbf344a4bcdd4030282cda3a36c557739fb56
Giovanni Sciortino <gsciorti@redhat.com>
2024-10-15 08:15:02 +0200

    Reorder script and configuration files based on their use in the procedure

--- 02_install_acm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ./python_venv/bin/activate
-ansible-playbook ./playbooks/install_acm.yml --extra-vars=@00_acm_configuration_variables.yml -v
--- 04_start_restore_to_secondary_hub.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ./python_venv/bin/activate
-ansible-playbook ./playbooks/start_restore_to_secondary_hub.yml -v --extra-vars=@00_acm_configuration_variables.yml 
